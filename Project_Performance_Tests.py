ef73b288e3974747167bb5f12f9a250c9cd8868a
RobertMcRae <robert.mcrae@mail.mcgill.ca>
2024-12-04 17:07:15 -0500

    Pytest implementation

--- Project_Performance_Tests.py
+++ Project_Performance_Tests.py
@@ -1,5 +1,5 @@
 import requests
-# import pytest
+import pytest
 # import pytest_randomly
 import time
 import numpy as np
@@ -7,8 +7,10 @@ import numpy as np
 BASE_URL = BASE_URL = "http://localhost:4567/"
 
 BASE_Project_URL = "http://localhost:4567/projects"
+n=1000
 
 #clear all items in the database, tearDown method validated - it works
+@pytest.fixture(autouse=True)
 def tearDown():
     response = requests.get(BASE_Project_URL)
     list_of_ids = []
@@ -23,7 +25,7 @@ def tearDown():
 
 #method to create a certain number of projects
 #n is the number of projects to create
-def create_n_projects(n: int):
+def test_create_n_projects():
     timeStamp = np.zeros(n)
     if not isinstance(n, int):
         raise ValueError("n must be an integer")
@@ -34,15 +36,19 @@ def create_n_projects(n: int):
         data = {}
         #create the random data (post)
         response = requests.post(BASE_Project_URL, json = data)
+        assert response.status_code == 201
         end_time = time.time()
         timeStamp[i] = end_time - start_time
         if response.status_code != 201: 
             raise ValueError(f"Status code not 201, {response.status_code}")
-    tearDown()
-    return timeStamp
+    #tearDown()
+
+    with open('creatingOutput.txt', 'w') as f:
+        f.write("Creating Projects time Marks\n")
+        f.write(f"{timeStamp}")
 
 #method to delete a certain number of projects
-def delete_n_projects(n: int):
+def test_delete_n_projects():
     timeStamp = np.zeros(n)
     if not isinstance(n, int):
         raise ValueError("n must be an integer")
@@ -63,14 +69,18 @@ def delete_n_projects(n: int):
     i = 0
     for id in list_of_ids:
         response = requests.delete(url.format(id = id))
+        assert response.status_code == 200
         end_time = time.time()
         timeStamp[i] = end_time - start_time
         i+=1
-    tearDown()
-    return timeStamp
+    #tearDown()
+
+    with open('deletingOutput.txt', 'w') as f:
+        f.write("Deleting Projects time Marks\n")
+        f.write(f"{timeStamp}")
 
 #method for changing a certain number of projects
-def change_n_projects(n: int):
+def test_change_n_projects():
     timeStamp = np.zeros(n)
     if not isinstance(n, int):
         raise ValueError("n must be an integer")
@@ -95,29 +105,32 @@ def change_n_projects(n: int):
         url = "http://localhost:4567/projects/{id}"
         data = {"title": "1"}
         time_start = time.time()
-        response = requests.post(url.format(id = id_to_look_for), json = data)
+        response = requests.put(url.format(id = id_to_look_for), json = data)
+        assert response.status_code == 200
         time_end = time.time()
         timeStamp[i] = time_end - time_start
 
-    tearDown()
-    return timeStamp
+    #tearDown()
+
+    with open('changeOutput.txt', 'w') as f:
+        f.write("Changing Projects time Marks\n")
+        f.write(f"{timeStamp}")
 
 
 
-#Result computing
-creating = create_n_projects(1000)
-deleting = delete_n_projects(1000)
-changing = change_n_projects(1000)
+# #Result computing
+# creating = create_n_projects(1000)
+# deleting = delete_n_projects(1000)
+# changing = change_n_projects(1000)
 
-#Reporting findings
-with open('output.txt', 'w') as f:
-    # Write each item in the list on a new line
-    f.write("Creating Projects time Marks\n")
-    for item in creating:
-        f.write(f"{item}\n")
-    f.write("Deleting Projects time Marks\n")
-    for item in deleting: 
-        f.write(f"{item}\n")
-    f.write("Changing Projects time Marks\n")
-    for item in changing: 
-        f.write(f"{item}\n")
+# #Reporting findings
+# with open('output.txt', 'w') as f:
+#     f.write("Creating Projects time Marks\n")
+#     for item in creating:
+#         f.write(f"{item}\n")
+#     f.write("Deleting Projects time Marks\n")
+#     for item in deleting: 
+#         f.write(f"{item}\n")
+#     f.write("Changing Projects time Marks\n")
+#     for item in changing: 
+#         f.write(f"{item}\n")
